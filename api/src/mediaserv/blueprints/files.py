64e9eb0794c01a1ff1efb2a710e7d228159f6856
Daniel Davella <danjo@danjomedia.org>
2022-07-29 21:06:59 -0400

    Intermediate progress towards scanning

--- api/src/mediaserv/blueprints/files.py
+++ api/src/mediaserv/blueprints/files.py
@@ -1,14 +1,22 @@
 import os
+from collections import defaultdict
+from pathlib import Path
 
+import audio_metadata as am
 from flask import Blueprint, current_app, request
 
+from ..models.db import db
 from ..models.album import Album
+from ..models.scans import Scan
 
 files = Blueprint("files", __name__, url_prefix="/files")
+
+EXTENSIONS_TO_SKIP = ['.png', '.jpg']
 
 
 @files.route("/", methods=["GET"])
 def get_files():
+    # TODO: use pathlib.Path here
     data_path = current_app.config.get("DATA_PATH")
     directory = request.args.get("directory", "")
     list_path = os.path.join(data_path, directory)
@@ -58,4 +66,40 @@ def get_files():
 
 @files.route("/scan", methods=["POST"])
 def scan_directory():
+    data_path = Path(current_app.config.get("DATA_PATH"))
+    directory = Path(request.args.get("directory", ""))
+    list_path = data_path / directory
+    current_app.logger.debug("list path: %s", list_path)
+
+    job_id = Scan.record_scan(directory)
+
+    # TODO: refactor into separate library module
+    albums = defaultdict(list)
+    for filename in list_path.rglob("*"):
+        if os.path.isdir(filename):
+            continue
+
+        # TODO: record image filenames with associated directories/albums
+        if filename.suffix in EXTENSIONS_TO_SKIP:
+            current_app.logger.debug("Skipping file because of extension: %s", filename)
+            continue
+
+        try:
+            meta = am.load(filename)
+        except am.exceptions.UnsupportedFormat as e:
+            current_app.logger.debug("%s: (filename=%s)", e, filename)
+            continue
+
+        if album_name := meta.tags.album[0]:
+            albums[album_name].append((filename, meta))
+
+    for album_name, tracks in albums.items():
+        for filename, meta in tracks:
+            pass
+
+    return {'job_id': job_id}
+
+@files.route("/scan/status", methods=["GET"])
+def scan_status():
+    job_id = request.args.get("job_id", "")
     return {}
--- api/src/mediaserv/models/album.py
+++ api/src/mediaserv/models/album.py
@@ -3,4 +3,4 @@ from .db import db
 
 class Album(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
